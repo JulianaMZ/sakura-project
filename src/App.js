dad1190c84769c30d437d93f847b07b44feb9c25
juliana montano <zjulim89@gmail.com>
2022-12-18 16:45:51 +0100

    ejercicio react

--- src/App.js
+++ src/App.js
@@ -1,24 +1,14 @@
 import './App.css';
+import Header from './components/Header';
 
 function App() {
+  
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='App'>
+      <h1>Hola</h1>
+      <Header message="Hola welcome props" name="Juliana"/>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
