97ac2ecd408247b9a55918d81d9b658e765656b4
juliana montano <zjulim89@gmail.com>
2022-12-20 21:41:48 +0100

    mayuscula css

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { Link } from "react-router-dom";
-import '../styles/Main.css';
+import '../styles/main.css';
+
 
 export default function Main({cards,setCards}) {
   
@@ -20,3 +21,14 @@ export default function Main({cards,setCards}) {
     </div>
   )
 }
+
+/*<>
+        <div className="group" id="card-color">
+            <div className="aspect-w-1 aspect-h-1 w-full overflow-hidden rounded-3xl bg-gray-200 xl:aspect-w-7 xl:aspect-h-8">
+                <ReactCardFlip isFlipped={isFlipped}>
+                    <img src ={backFace} alt ='back face card' className="h-full w-full object-cover object-center group-hover:opacity-75 rounded-3xl" onClick={handleClick}/>
+                    <img key = {key} src ={image} alt ={name} className="h-full w-full object-cover object-center group-hover:opacity-75 rounded-3xl" onClick={handleClick}/>
+                </ReactCardFlip>
+            </div>
+        </div>
+    </>*/
