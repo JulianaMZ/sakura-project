6bb1dad64b482010d46d143cecaa6e331fcc57b0
juliana montano <zjulim89@gmail.com>
2022-12-21 23:38:47 +0100

    cambios que no sirven

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,19 +1,24 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import '../styles/main.css';
+import ReactCardFlip from 'react-card-flip';
 
 
 export default function Main({cards,setCards}) {
   
   console.log(cards);
-
+const Main = ({cardNumber, spanishName, clowCard}) => {
+  const [isFlipped, setIsFlipped] = useState(false);
+}
   return (
     <div className="cards">
       <h1>Escoge 3 cartas</h1>
         <div className="container-cards">
         {cards.map((card, index) => (
           <div className="card-container" key={index}>
-            <img src={card.cardsReverse.clowReverse}alt={card.spanisName}/>
+            <ReactCardFlip isFlipped={isFlipped} >
+              <img src={card.cardsReverse.clowReverse}alt={card.spanisName} />
+            </ReactCardFlip>
           </div>
         ))}
       </div>
